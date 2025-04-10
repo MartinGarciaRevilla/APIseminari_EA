d93809c24a9f0cca82f449929768e32ba53ac669
MartinGarciaRevilla <martin.garcia.revilla@estudiantat.upc.edu>
2025-04-10 11:19:41 +0200

    ejercicio react hehco

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,19 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './modules/users/user_routes.js'; // Nota el .js al final
 import forumRoutes from './modules/forum/forum_routes.js'; // Nota el .js al final
-import subjectRoutes from './modules/subject/subject_routes.js'; // Nota el .js al final
+import subjectRoutes from './modules/subject/subject_routes.js';
 import { corsHandler } from './middleware/corsHandler.js';
 import { loggingHandler } from './middleware/loggingHandler.js';
 import { routeNotFound } from './middleware/routeNotFound.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
+import { ensureDefaultUser } from './modules/users/user_service.js';
 
 dotenv.config(); // Cargamos las variables de entorno desde el archivo .env
 
 const app = express();
 
-const LOCAL_PORT = process.env.SERVER_PORT || 8000;
+const LOCAL_PORT = process.env.SERVER_PORT || 9000;
 
 // Configuración de Swagger
 const swaggerOptions = {
@@ -27,16 +28,16 @@ const swaggerOptions = {
         },
         tags: [
             {
+                name: 'Subjects',
+                description: 'Rutas relacionadas con la gestión de asignaturas',
+            },
+            {
               name: 'Users',
               description: 'Rutas relacionadas con la gestión de usuarios',
             },
             {
               name: 'Forum',
               description: 'Rutas relacionadas con el forum',
-            },
-            {
-              name: 'Subject',
-              description: 'Rutas relacionadas con las asignaturas',
             },
             {
               name: 'Main',
@@ -49,7 +50,7 @@ const swaggerOptions = {
             }
         ]
     },
-    apis: ['./modules/users/*.js', './modules/forum/*.js', './modules/subject/*.js'] // Asegúrate de que esta ruta apunta a tus rutas
+    apis: ['./build/modules/users/*.js', './build/modules/forum/*.js','./build/modules/subject/*.js'] // Asegúrate de que esta ruta apunta a tus rutas
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
@@ -60,29 +61,21 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use(loggingHandler);
 app.use(corsHandler);
-
-// Middleware para registrar todas las solicitudes
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
-
-// Rutas
+//rutas
 app.use('/api', userRoutes);
 app.use('/api', forumRoutes);
 app.use('/api', subjectRoutes);
-
-// Ruta de prueba
+// Rutes de prova
 app.get('/', (req, res) => {
     res.send('Welcome to my API');
 });
 
-// Middleware para manejar rutas no encontradas
-app.use(routeNotFound);
+ensureDefaultUser();
 
 // Conexión a MongoDB
+//mongoose;
 mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb+srv://joan:1234@cluster0.3owhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/backEndReact')
     .then(() => console.log('Connected to DB'))
     .catch((error) => console.error('DB Connection Error:', error));
 
